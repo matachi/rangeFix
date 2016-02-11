061568b86838b2761c7470db635f6c6c2217a668
Daniel Jonsson <daniel@jonsson.xyz>
2016-02-11 17:15:13 +0100

    Make paths relative in runKconfig.sh

--- runKconfig.sh
+++ runKconfig.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp "/home/matachi/dev/vm/rangeFix/target/scala-2.9.2/classes:/home/matachi/dev/vm/rangeFix/lib/kiama_2.9.2-1.4.0.jar:/home/matachi/dev/vm/rangeFix/lib/scala-library.jar:/home/matachi/dev/vm/rangeFix/lib/lvat-0.5-SNAPSHOT.jar" ca.uwaterloo.gsd.rangeFix.KconfigMain $1 $2 $3 $4
+java -cp "target/scala-2.9.2/classes:lib/kiama_2.9.2-1.4.0.jar:lib/scala-library.jar:lib/lvat-0.5-SNAPSHOT.jar" ca.uwaterloo.gsd.rangeFix.KconfigMain $1 $2 $3 $4
