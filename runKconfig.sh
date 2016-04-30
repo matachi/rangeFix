8c8934f9f91ac843631d3b9e53530feae2324408
Daniel Jonsson <daniel@jonsson.xyz>
2016-04-30 16:54:30 +0200

    Change how args are gotten in runKconfig.sh

--- runKconfig.sh
+++ runKconfig.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp "target/scala-2.9.2/classes:lib/kiama_2.9.2-1.4.0.jar:lib/scala-library.jar:lib/lvat-0.5-SNAPSHOT.jar" ca.uwaterloo.gsd.rangeFix.KconfigMain $1 $2 $3 $4
+java -cp "target/scala-2.9.2/classes:lib/kiama_2.9.2-1.4.0.jar:lib/scala-library.jar:lib/lvat-0.5-SNAPSHOT.jar" ca.uwaterloo.gsd.rangeFix.KconfigMain $@
